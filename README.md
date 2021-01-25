a240c754691bcb1a180b151e0c7ff2f19eb5d59c
Elvin Chu <elvincth@gmail.com>
2021-01-25 11:56:45 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# vue3-ionic-pwa
+ ionic-5 vue3 starter with pwa and tailwind setup 
---
+++ src/views/Tab1.vue
@@ -0,0 +1,58 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Tab 1</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Calculate</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      <div class="container mx-auto">
+        <ion-card>
+          <ion-card-header>
+            <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
+            <ion-card-title class="font-semibold">Test</ion-card-title>
+          </ion-card-header>
+
+          <ion-card-content> 00B </ion-card-content>
+        </ion-card>
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonCard,
+  IonCardContent,
+  IonCardSubtitle,
+  IonCardHeader,
+  IonCardTitle,
+} from "@ionic/vue";
+
+export default {
+  name: "Tab1",
+  components: {
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonPage,
+    IonCard,
+    IonCardContent,
+    IonCardSubtitle,
+    IonCardTitle,
+    IonCardHeader,
+  },
+};
+</script>
